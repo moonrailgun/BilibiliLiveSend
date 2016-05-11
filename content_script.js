bd87a749412c97dd28003841d23fc73fc0ed7b5e
moonrailgun <moonrailgun@gmail.com>
2016-05-11 11:13:07 +0800

    弹幕发送
    ADD 单条弹幕发送
    ADD 多条弹幕发送
    ADD 内置200ms延时
    UPDATE 闭包封装
    UPDATE 原生发送弹幕方法

--- content_script.js
+++ content_script.js
@@ -1,44 +1,61 @@
 /**
  * Created by Chen on 2016-05-10.
  */
+$(function () {
+    var sendDanmuOnce = function (msg) {
+        var color = '0xffffff';//默认颜色
+        if (msg != '') {
+            msg = $.trim(msg);//去除首尾空格
+            $("#player_object")[0].sendMsg(msg, color);//调用播放器的方法发送弹幕
+            console.log("弹幕消息发送完毕");
+        } else {
+            console.log("弹幕消息内容为空");
+        }
+    };
 
-console.log("BilibiliLiveSend 正在运行");
+    var restDanmuTaskNum = 0;
+    var sendDanmuMultipleTimes = function (msg) {
+        setTimeout(function () {
+            sendDanmuOnce(msg);
+            restDanmuTaskNum--;
+            if (restDanmuTaskNum > 0) {
+                sendDanmuMultipleTimes(msg);//迭代
+            }
 
-var sendDanmuOnce = function(){
+        }, 200);
+    };
 
-};
+    var addHtml = function () {
+        var content = '';
+        content += '<div class="clear-float danmu-sender" style="width: 300px;">' +
+            '<textarea class="danmu-textbox float-left bls-sendInput" spellcheck="false" placeholder="请输入弹幕DA☆ZE～" maxlength="20"></textarea>' +
+            '<button class="danmu-send-btn float-right live-btn default bls-sendOnce" role="button" aria-label="点击发送弹幕" style="width: 55px;">发送一次</button></div>';
 
-var addHtml = function() {
-    var content = '';
-    content += '<div class="clear-float danmu-sender" style="width: 300px;">' +
-        '<textarea class="danmu-textbox float-left bls-sendInput" spellcheck="false" placeholder="请输入弹幕DA☆ZE～" maxlength="20"></textarea>' +
-        '<button class="danmu-send-btn float-right live-btn default bls-sendOnce" role="button" aria-label="点击发送弹幕" style="width: 55px;">发送一次</button></div>';
+        content += '<div class="clear-float danmu-sender" style="width:300px;">' +
+            '<input type="number" class="danmu-textbox float-left bls-sendMultipleTimes" placeholder="连续发送多少次DA☆ZE～" style="height: auto;"/>' +
+            '<button class="danmu-send-btn float-right live-btn default bls-sendMultiple" role="button" aria-label="点击发送弹幕" style="width: 55px;height: auto;">连续发送</button></div>';
 
-    var html = '<div id="bls" class="main-section room-intro" ms-controller="roomIntroCtrl"><div class="section-header clear-float">' +
-        '<h2 class="section-title">B站直播刷屏姬 - BilibiliLiveSend</h2></div><div class="content-container" ms-html="roomIntro">' +
-        '<p>'+content+'</p></div></div>';
 
-    $('.room-intro:last').before(html);
-};
+        var html = '<div id="bls" class="main-section room-intro" ms-controller="roomIntroCtrl"><div class="section-header clear-float">' +
+            '<h2 class="section-title">B站直播刷屏姬 - BilibiliLiveSend</h2></div><div class="content-container" ms-html="roomIntro">' +
+            '<p>' + content + '</p></div></div>';
 
-addHtml();
+        $('.room-intro:last').before(html);
+    };
 
-$(function(){
-    console.log($('#bls .bls-sendOnce'));
-    $('#bls .bls-sendOnce').click(function(){
+
+    addHtml();
+    $('#bls .bls-sendOnce').click(function () {
         var msg = $('#bls .bls-sendInput').val();
-        var rnd = Math.round(new Date().getTime()/1000);
-        //var data = 'color=16777215&fontsize=25&mode=1&msg=%E4%B8%87%E4%B8%80%E4%B8%AD%E4%BA%86%E5%91%A2&rnd=1462888732&roomid=5440';
-        /*
-        $.post('http://live.bilibili.com/msg/send',{
-            color:16777215,
-            fontsize:25,
-            mode:1,
-            msg:msg,
-            rnd:rnd,
-            roomid:5440
-        }, function(data,status){
-            console.log(data);
-        })*/
+        sendDanmuOnce();
     });
+    $('#bls .bls-sendMultiple').click(function () {
+        var times = $('#bls .bls-sendMultipleTimes').val();
+        var msg = $('#bls .bls-sendInput').val() || 0;
+        times = Number(times);
+        restDanmuTaskNum = times;
+        sendDanmuMultipleTimes(msg);
+    });
+
+    console.log("BilibiliLiveSend 正在运行");
 });
